0f62b1963cec119b033b49ab9af9165d87a01274
whiffy <bryanzhao168@yahoo.ca>
2025-10-04 14:58:23 -0400

    Added new dependencies and config file to allow photos from CDN

--- front_end/query-front-end/next.config.ts
+++ front_end/query-front-end/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["d385y5zl1bby6g.cloudfront.net"],
+  },
 };
 
 export default nextConfig;
